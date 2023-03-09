43e3b1776aedfdcdf90d100e92eaab0b7e2d7158
nkmni <7596222+nkmni@users.noreply.github.com>
2023-03-08 20:13:45 -0800

    added worker logging

--- src/worker.ts
+++ src/worker.ts
@@ -4,16 +4,26 @@ import crypto from 'crypto';
 import { canonicalize } from 'json-canonicalize';
 import { hash } from './crypto/hash';
 import { TARGET } from './block';
+import { logger } from './logger';
 
 function main() {
+  let hashes = 0;
+  const startTime = Date.now();
+  let prevTotalSeconds = 0;
   const candidateBlock: BlockObjectType = workerData;
   while (true) {
+    let totalSeconds = (Date.now() - startTime) / 1000;
+    if (totalSeconds - prevTotalSeconds > 5) {
+      prevTotalSeconds = totalSeconds;
+      logger.log('debug', `hashrate: ${hashes / totalSeconds}`);
+    }
     candidateBlock.nonce = crypto.randomBytes(32).toString('hex');
     const candidateBlockId = hash(canonicalize(candidateBlock));
     if (BigInt(`0x${candidateBlockId}`) <= BigInt(`0x${TARGET}`)) {
       parentPort?.postMessage(candidateBlock);
       break;
     }
+    ++hashes;
   }
 }
 
